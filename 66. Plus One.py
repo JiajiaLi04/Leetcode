35bac67fedb19fbdaf14af880fd30e9719705979
Li, Jiajia <lijiajia@msu.edu>
2020-08-14 08:56:00 -0400

    update 66

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -1,13 +1,3 @@
-# class Solution:
-#     def plusOne(self, digits: List[int]) -> List[int]:
-#         i = len(digits)-1
-#         if i>0:
-#             digits[i] +=1
-#             digits[i] = digits[i]%10
-#             if digits[i]!=0:
-#                 return digits
-#             i = i-1
-
 class Solution:
     def plusOne(self, digits):
         for i in range(len(digits)-1, -1, -1):
@@ -20,6 +10,6 @@ class Solution:
  
 if __name__ == '__main__':
     func = Solution()
-    nums = [9,0,9,0,9]
+    nums = [9,0,9,9,9]
     print(func.plusOne(nums))
         
