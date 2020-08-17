c5535f291d74e30de48103748e9b1c25d3638106
Li, Jiajia <lijiajia@msu.edu>
2020-08-17 14:49:19 -0400

    add files

---
+++ 136. Single Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums) -> int:
+        r = 0
+        for i in nums:
+            r ^= i
+        return r
+
+
+if __name__ == '__main__':
+    func = Solution()
+    nums = [9,1,9,2,2]
+    print(func.singleNumber(nums))
+        
