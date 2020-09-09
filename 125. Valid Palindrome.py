c08cee6381e52aa67286889e0c8f8d42cd5cf50f
Li, Jiajia <lijiajia@msu.edu>
2020-09-08 23:30:06 -0400

    update 125

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -2,14 +2,14 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         for ch in s:
             if ch.isalnum():
-                A = ch.lower()
-        sgood = "".join(A)
+                sgood = "".join(ch.lower())
         # sgood = "".join(ch.lower() for ch in s if ch.isalnum())
-        return sgood == sgood[::-1]
+        return sgood == sgood[::-1] ## [::-1] to do the reverse for tuples, arrays, and strings
 
 
 if __name__ == '__main__':
-    s = "A man, a plan, a canal: Panama"
+    # s = "A man, a plan, a canal: Panama"
+    s = "A man, a plan, a canal: Panam"
     func = Solution()
     output = func.isPalindrome(s)
     print(output)
