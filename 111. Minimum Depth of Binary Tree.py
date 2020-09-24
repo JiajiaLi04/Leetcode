10d16c47dd34875705df0acf23267e78020f3d97
lijiajia <lijiajia@example.com>
2020-09-23 21:21:18 -0400

    updates

--- 111. Minimum Depth of Binary Tree.py
+++ 111. Minimum Depth of Binary Tree.py
@@ -5,41 +5,24 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
-# class Solution:
-#     def minDepth(self, root: TreeNode) -> int:
-#         if root==None:
-#             return 0
-
-#         def node_depth(root1):
-#             if root1==None:
-#                 return  0
-#             if root1:
-#                 if (root1.left!=None or root1.right!=None):
-#                     return 2
-#                 else:
-#                     return 1
-#             return  min(node_depth(root1.left), node_depth(root1.right))
-                   
-#         return node_depth(root) + 1
-
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
-        
+        # 这道题递归条件里分为三种情况
+        # 1.左孩子和有孩子都为空的情况，说明到达了叶子节点，直接返回1即可
         if not root.left and not root.right:
             return 1
-        
+   
         min_depth = 10**9
-        if root.left:
-            min_depth = min(self.minDepth(root.left), min_depth)
-        if root.right:
-            min_depth = min(self.minDepth(root.right), min_depth)
-        
-        return min_depth + 1
-
-        
+        #2.如果左孩子和由孩子其中一个为空，那么需要返回比较大的那个孩子的深度        
+        m1 = min_depth(root.left)
+        m2 = min_depth(root.right)
+        # 这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
+        if root.left==None or root.right==None:
+            return m1 + m2 +1
+        # 3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
+        return min_depth +1    
 
 if __name__ == '__main__':
     p = TreeNode(1)
