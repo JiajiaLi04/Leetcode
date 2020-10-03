57b267c3a4d72276bb5913e7a757e4556902eef4
JiajiLi04 <jiajiali_@outlook.com>
2020-10-02 23:42:54 -0400

    slicing windows

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,27 +1,45 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s:return 0
+        if not s:
+            return 0
         left = 0
-        # 哈希集合，记录每个字符是否出现过
-        lookup = set()
-        n = len(s)
-        max_len = 0
-        cur_len = 0
-        for i in range(n):
-            cur_len += 1
-            ## check whether current element is in hashset.
-            while s[i] in lookup:
-                lookup.remove(s[left])
-                left += 1
-                cur_len -= 1
-            if cur_len > max_len:max_len = cur_len
-            lookup.add(s[i])
-        return max_len
+        curr_length = 0
+        max_length = 0
+        str_set = set()
+        for i in range(len(s)):
+            while s[i] in str_set:           
+                str_set.remove(s[left])
+                left = left+1
+            str_set.add(s[i])
+            curr_length = i - left + 1
+            max_length = max(max_length, curr_length)
+        return max_length
+            
+## solution from others
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         if not s:return 0
+#         left = 0
+#         # 哈希集合，记录每个字符是否出现过
+#         lookup = set()
+#         n = len(s)
+#         max_len = 0
+#         cur_len = 0
+#         for i in range(n):
+#             cur_len += 1
+#             ## check whether current element is in hashset.
+#             while s[i] in lookup:
+#                 lookup.remove(s[left])
+#                 left += 1
+#                 cur_len -= 1
+#             if cur_len > max_len:max_len = cur_len
+#             lookup.add(s[i])
+#         return max_len
 
 
 
         
 if __name__ == '__main__':
     func = Solution()
-    s = "abcabcbb"
+    s = 'pwwkew'
     print(func.lengthOfLongestSubstring(s))
--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -1,37 +1,89 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t)>len(s):
-            return ' '
+            return ''
         left_index = 0
-        # right_index = 0
-        curr_index = 0
+        index_record = []
+        min_length = 10e9
+        counter = 0
+
         for i in range(len(s)):
-            curr_index  = len(s) + 1
-            if curr_index
+            curr_string = s[left_index:i+1]
+            
+            for i in range(len(t)):
+                if t[i] not in curr_string:
+                    counter= counter+1
+            if counter==0:
+            
+            # while set(t).issubset(set(curr_string)):
+                if (i-left_index+1)<min_length:
+                    min_length = i-left_index+1
+                    index_record = [left_index, i]
+                if left_index>=len(s)-1:
+                    break
+                # index_record.append([left_index, i])
+                left_index = left_index + 1
+                curr_string = s[left_index:i+1]
+
+                if left_index>=len(s)-1:
+                    continue
+                while s[left_index] not in t:
+                    left_index = left_index + 1
+                    curr_string = s[left_index:i+1]
+        if len(index_record)==0:
+            return ''
+        else:
+            return s[index_record[0]: index_record[1]+1]
+        # return ' '
 
 
+        # if len(index_record):
+        #     length_min = []
+        #     for i in range(len(index_record)):
+        #         length_min.append(index_record[i][1] - index_record[i][0])
 
-        if not s:return 0
-        left = 0
-        # 哈希集合，记录每个字符是否出现过
-        lookup = set()
-        n = len(s)
-        max_len = 0
-        cur_len = 0
-        for i in range(n):
-            cur_len += 1
-            ## check whether current element is in hashset.
-            while s[i] in lookup:
-                lookup.remove(s[left])
-                left += 1
-                cur_len -= 1
-            if cur_len > max_len:max_len = cur_len
-            lookup.add(s[i])
-        return max_len
+        #     index = min(length_min)
+        # else:
+        #     return ' '
+            
+                
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if len(t)>len(s):
+#             return ' '
+#         left_index = 0
+#         index_record = []
+#         min_length = 10e9
+#         for i in range(len(s)):
+#             curr_string = s[left_index:i+1]
+#             while set(t).issubset(set(curr_string)):
+#                 index_record.append([left_index, i])
+#                 left_index = left_index + 1
+#                 curr_string = s[left_index:i+1]
+
+#                 while s[left_index] not in t:
+#                     left_index = left_index + 1
+#                     curr_string = s[left_index:i+1]
+
+
+#         if len(index_record):
+#             length_min = []
+#             for i in range(len(index_record)):
+#                 length_min.append(index_record[i][1] - index_record[i][0])
+
+#             index = min(length_min)
+#         else:
+#             return ' '
+            
+                
+
 
 
 if __name__ == '__main__':
     func = Solution()
-    S = "ADOBECODEBANC"
-    T = "ABC"
+    # S = "ADOBECODEBANC"
+    # T = "ABC"
+    S = "ab"
+    T = "b"
     print(func.minWindow(S, T))
