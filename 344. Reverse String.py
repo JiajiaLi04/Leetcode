5c277f37852f148b103db2c93e45aaeb3078dc7c
JiajiLi04 <jiajiali_@outlook.com>
2020-12-15 10:15:16 -0500

    update

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -5,22 +5,28 @@
 #             if left < right:
 #                 s[left], s[right] = s[right], s[left]
 #                 helper(left + 1, right - 1)
-
 #         helper(0, len(s) - 1)
 
 
-## Two pointers
-class Solution:
-    def reverseString(self, s):
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left, right = left + 1, right - 1
+# ## Two pointers
+# class Solution:
+#     def reverseString(self, s):
+#         left, right = 0, len(s) - 1
+#         while left < right:
+#             s[left], s[right] = s[right], s[left]
+#             left, right = left + 1, right - 1
 
 
+# if __name__ == '__main__':
+# 	s = ["h","e","l","l","o"]
+# 	func = Solution()
+# 	func.reverseString(s)
+# 	print(s)
 
-if __name__ == '__main__':
-	s = ["h","e","l","l","o"]
-	func = Solution()
-	func.reverseString(s)
-	print(s)
+
+## OJ input and output model
+# data_input = input()
+num = [i for i in input()] 
+num.reverse() 
+num = ''.join(num) 
+print(num)
