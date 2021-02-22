37f9367591b7610368d9069fbff5416b2c44dbd7
lijiajia <lijiajia04@outlook.com>
2021-02-22 17:58:00 +0800

    updates

--- 637.averageOfLevels.py
+++ 637.averageOfLevels.py
@@ -7,4 +7,18 @@ class TreeNode:
 
 class Solution:
     def averageOfLevels(self, root: TreeNode) -> List[float]:
-        
+
+
+if __name__ == '__main__':
+    p = TreeNode(3)
+    p1 = TreeNode(9)
+    p2 = TreeNode(20)
+    p3 = TreeNode(15)
+    p4 = TreeNode(7)
+    p.left = p1
+    p.right = p2
+    p2.left = p3
+    p2.right = p4
+
+    func = Solution()
+    print(func.averageOfLevels(p))
