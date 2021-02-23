4686d94c7210d5fb2073c1ea95943c1f218fb9fc
lijiajia <lijiajia04@outlook.com>
2021-02-23 16:34:12 +0800

    updates

--- 637.averageOfLevels.py
+++ 637.averageOfLevels.py
@@ -7,6 +7,22 @@ class TreeNode:
 
 class Solution:
     def averageOfLevels(self, root: TreeNode) -> List[float]:
+        def dfs(root: TreeNode, level: int):
+            if not root:
+                return
+            if level < len(totals):
+                totals[level] += root.val
+                counts[level] += 1
+            else:
+                totals.append(root.val)
+                counts.append(1)
+            dfs(root.left, level + 1)
+            dfs(root.right, level + 1)
+
+        counts = list()
+        totals = list()
+        dfs(root, 0)
+        return [total / count for total, count in zip(totals, counts)]
 
 
 if __name__ == '__main__':
